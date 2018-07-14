24921b1fdd05a91eb470501cf0dcd094f6b5ee5a
Nikita Bobko <nikitabobko+git@gmail.com>
2018-07-15 00:19:56 +0300

    First release preparations
    Includes ArchLinux PKGBUILD, updated makedfile, included uninstall
    script

--- support/install.sh
+++ support/install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
-mkdir -p /opt/gcal-notifier-kotlin-gtk
-cp gcal-notifier-kotlin-gtk.jar /opt/gcal-notifier-kotlin-gtk/gcal-notifier-kotlin-gtk.jar
-cp icon.png /opt/gcal-notifier-kotlin-gtk/icon.png
-cp launcher.sh /usr/bin/gcal-notifier-kotlin-gtk
-cp desktop-entry.desktop /usr/share/applications/gcal-notifier-kotlin-gtk.desktop
+root=/
+if [ "$#" == 1 ]; then
+    root=$1
+fi
+
+install -D -m644 gcal-notifier-kotlin-gtk.jar ${root}/opt/gcal-notifier-kotlin-gtk/gcal-notifier-kotlin-gtk.jar
+install -D -m644  icon.png ${root}/opt/gcal-notifier-kotlin-gtk/icon.png
+install -D launcher.sh ${root}/usr/bin/gcal-notifier-kotlin-gtk
+install -D -m644 desktop-entry.desktop ${root}/usr/share/applications/gcal-notifier-kotlin-gtk.desktop
+install -D .uninstall-1.0.sh ${root}/opt/gcal-notifier-kotlin-gtk/.uninstall-1.0.sh
+install -D uninstall.sh ${root}/opt/gcal-notifier-kotlin-gtk/uninstall.sh
