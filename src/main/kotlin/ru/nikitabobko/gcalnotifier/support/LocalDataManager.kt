81bdf948670d1c2bb3e6909e6783998292c5a850
Nikita Bobko <nikitabobko+git@gmail.com>
2018-07-21 11:24:55 +0300

    Remove all user sensetive data when log out

--- src/main/kotlin/ru/nikitabobko/gcalnotifier/support/LocalDataManager.kt
+++ src/main/kotlin/ru/nikitabobko/gcalnotifier/support/LocalDataManager.kt
@@ -11,65 +11,54 @@ import java.io.*
 interface LocalDataManager {
     /**
      * Path to directory where Google Calendar API stores credentials
-     * @see removeGoogleCalendarCredentialsDir
      */
     val googleCalendarCredentialsDirPath: String
 
-    /**
-     * Remove directory where Google Calendar API stores credentials
-     * @see googleCalendarCredentialsDirPath
-     */
-    fun removeGoogleCalendarCredentialsDir()
     fun safeEventsList(events: Array<MyEvent>)
-    /**
-     * @throws FileNotFoundException
-     */
+
     @Throws(FileNotFoundException::class)
     fun restoreEventsList(): Array<MyEvent>
+
     fun safeUsersCalendarList(calendarList: Array<MyCalendarListEntry>)
-    /**
-     * @throws FileNotFoundException
-     */
+
     @Throws(FileNotFoundException::class)
     fun restoreUsersCalendarList(): Array<MyCalendarListEntry>
+
     fun safe(events: Array<MyEvent>, calendarList: Array<MyCalendarListEntry>)
+
+    fun removeAllData()
 }
 
 /**
  * Implementation based on JSON
  */
 class LocalDataManagerJSON : LocalDataManager {
-    override val googleCalendarCredentialsDirPath: String
-        get() = "$USER_HOME_FOLDER/.config/$APPLICATION_NAME/credentials"
+    private val localDataFolderPath = "$USER_HOME_FOLDER/.config/$APPLICATION_NAME"
+    private val eventsListFileLocation = "$localDataFolderPath/events.json"
+    private val userCalendarFileLocation = "$localDataFolderPath/calendars.json"
+    override val googleCalendarCredentialsDirPath = "$localDataFolderPath/credentials"
     private val gson = Gson()
     private val lock = Any()
-    companion object {
-        private val EVENTS_LIST_FILE_LOCATION =
-                "$USER_HOME_FOLDER/.config/$APPLICATION_NAME/events.json"
-
-        private val USER_CALENDAR_LIST_FILE_LOCATION =
-                "$USER_HOME_FOLDER/.config/$APPLICATION_NAME/calendars.json"
-    }
 
     override fun safe(events: Array<MyEvent>, calendarList: Array<MyCalendarListEntry>) {
         safeEventsList(events)
         safeUsersCalendarList(calendarList)
     }
 
-    override fun safeEventsList(events: Array<MyEvent>) = safe(events, EVENTS_LIST_FILE_LOCATION)
+    override fun safeEventsList(events: Array<MyEvent>) = safe(events, eventsListFileLocation)
 
     override fun safeUsersCalendarList(calendarList: Array<MyCalendarListEntry>) =
-            safe(calendarList, USER_CALENDAR_LIST_FILE_LOCATION)
+            safe(calendarList, userCalendarFileLocation)
 
 
     override fun restoreEventsList(): Array<MyEvent> =
-            restore(EVENTS_LIST_FILE_LOCATION) ?: throw FileNotFoundException()
+            restore(eventsListFileLocation) ?: throw FileNotFoundException()
 
     override fun restoreUsersCalendarList(): Array<MyCalendarListEntry> =
-            restore(USER_CALENDAR_LIST_FILE_LOCATION) ?: throw FileNotFoundException()
+            restore(userCalendarFileLocation) ?: throw FileNotFoundException()
 
-    override fun removeGoogleCalendarCredentialsDir() {
-        File(googleCalendarCredentialsDirPath).deleteRecursively()
+    override fun removeAllData() {
+        File(localDataFolderPath).deleteRecursively()
     }
 
     private fun safe(any: Any, fileName: String) {
