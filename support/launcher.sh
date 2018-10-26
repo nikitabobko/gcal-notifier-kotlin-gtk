f49cf69f983dbc4b30144187027d21548e1b4672
Nikita Bobko <nikitabobko@gmail.com>
2018-10-26 23:04:32 +0300

    Fix very high CPU usage. Add more parameters for controlling memory on JVM

--- support/launcher.sh
+++ support/launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 java -cp /usr/share/java/gtk.jar:/opt/gcal-notifier-kotlin-gtk/gcal-notifier-kotlin-gtk.jar \
--Xmx8m -Xss228k ru.nikitabobko.gcalnotifier.MainKt $@
+-Xmx8m -Xms8m -Xss228k ru.nikitabobko.gcalnotifier.MainKt $@
