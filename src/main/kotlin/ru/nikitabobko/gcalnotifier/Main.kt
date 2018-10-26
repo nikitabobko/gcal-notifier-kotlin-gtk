d4a6cd78d28a7e8e22eaa1a1b2d09e2174707d03
Nikita Bobko <nikitabobko+git@gmail.com>
2018-10-26 23:08:46 +0300

    v2.0.1
    Changelog:
     * Fix high CPU usage bug

--- src/main/kotlin/ru/nikitabobko/gcalnotifier/Main.kt
+++ src/main/kotlin/ru/nikitabobko/gcalnotifier/Main.kt
@@ -6,7 +6,7 @@ import ru.nikitabobko.gcalnotifier.support.MyFactory
 import kotlin.system.exitProcess
 
 const val APPLICATION_NAME = "gcal-notifier-kotlin-gtk"
-const val APPLICATION_VERSION = "v2.0"
+const val APPLICATION_VERSION = "2.0.1"
 val UI_THREAD_ID = Thread.currentThread().id
 
 /**
