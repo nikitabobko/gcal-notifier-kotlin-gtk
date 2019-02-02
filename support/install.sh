7cf71b825d9b15dce3e9900df3fb951a392b9785
Nikita Bobko <nikitabobko+git@gmail.com>
2019-02-03 00:18:55 +0300

    Add debian package build into makefile. Remove .uninstall-1.0.sh

--- support/install.sh
+++ support/install.sh
@@ -4,9 +4,10 @@ if [ "$#" -eq 1 ]; then
     root=$1
 fi
 
-install -D -m644 gcal-notifier-kotlin-gtk.jar ${root}/opt/gcal-notifier-kotlin-gtk/gcal-notifier-kotlin-gtk.jar
-install -D -m644  icon.png ${root}/opt/gcal-notifier-kotlin-gtk/icon.png
-install -D launcher.sh ${root}/usr/bin/gcal-notifier-kotlin-gtk
-install -D -m644 desktop-entry.desktop ${root}/usr/share/applications/gcal-notifier-kotlin-gtk.desktop
-install -D .uninstall-1.0.sh ${root}/opt/gcal-notifier-kotlin-gtk/.uninstall-1.0.sh
-install -D uninstall.sh ${root}/opt/gcal-notifier-kotlin-gtk/uninstall.sh
+cur=$(dirname $0)
+
+install -D -m644 ${cur}/gcal-notifier-kotlin-gtk.jar ${root}/opt/gcal-notifier-kotlin-gtk/gcal-notifier-kotlin-gtk.jar
+install -D -m644  ${cur}/icon.png ${root}/opt/gcal-notifier-kotlin-gtk/icon.png
+install -D ${cur}/launcher.sh ${root}/usr/bin/gcal-notifier-kotlin-gtk
+install -D -m644 ${cur}/desktop-entry.desktop ${root}/usr/share/applications/gcal-notifier-kotlin-gtk.desktop
+install -D ${cur}/uninstall.sh ${root}/opt/gcal-notifier-kotlin-gtk/uninstall.sh
--- support/uninstall.sh
+++ support/uninstall.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
-./.uninstall-1.0.sh
+rm -f /usr/bin/gcal-notifier-kotlin-gtk
+rm -f /usr/share/applications/gcal-notifier-kotlin-gtk.desktop
+rm -rf $HOME/.config/gcal-notifier-kotlin-gtk/
 rm -rf /opt/gcal-notifier-kotlin-gtk
