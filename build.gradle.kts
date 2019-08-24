84ba88ddcc174032ad59e6967fd89d5b28db3d06
Nikita Bobko <nikitabobko+git@gmail.com>
2019-08-25 02:54:20 +0300

    Migrate makefile tasks into gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-buildscript {
-    extra.apply {
-        set("kotlin_version", "1.3.21")
-    }
+val mainClassName = "ru.nikitabobko.gcalnotifier.MainKt"
+val appName = "gcal-notifier-kotlin-gtk"
+val appVersion = rootProject.file("src/main/resources/version.txt").readText().trim()
 
+buildscript {
     repositories {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra.get("kotlin_version")}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
     }
 }
 
 plugins {
     id("java")
     kotlin("jvm") version "1.3.21"
-    id("application")
-}
-
-group = "ru.nikitabobko"
-version = rootProject.file("src/main/resources/version.txt").readText()
-
-application {
-    mainClassName = "ru.nikitabobko.gcalnotifier.MainKt"
 }
 
 repositories {
@@ -46,13 +38,13 @@ dependencies {
     compile(group = "", name = "gtk")
 
     // Test dependencies
-    testCompile("org.jetbrains.kotlin:kotlin-test-junit:${extra.get("kotlin_version")}")
+    testCompile("org.jetbrains.kotlin:kotlin-test-junit:1.3.21")
     testCompile("org.mockito:mockito-core:2.1.0")
 }
 
-val jar by tasks.getting(Jar::class) {
+val jar = tasks.getByName("jar", type = Jar::class) {
     manifest {
-        attributes(mapOf("Main-Class" to application.mainClassName))
+        attributes(mapOf("Main-Class" to mainClassName))
     }
     from(configurations.compile.filter { !it.name.contains("gtk") }.map {
         @Suppress("IMPLICIT_CAST_TO_ANY")
@@ -61,8 +53,10 @@ val jar by tasks.getting(Jar::class) {
 }
 
 task("runJar", type = Exec::class) {
-    executable("java")
-    args("-cp", "${jar.archivePath}:/usr/share/java/gtk.jar", application.mainClassName)
+    group = "Run"
+    description = "Run compiled jar."
+    setDependsOn(listOf("jar"))
+    commandLine = listOf("java", "-cp", "${jar.archivePath}:/usr/share/java/gtk.jar", mainClassName)
 }
 
 tasks.withType<KotlinCompile> {
@@ -71,4 +65,78 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     testLogging.showStandardStreams = true
+}
+
+open class BashExec : DefaultTask() {
+    var command: String? = null
+
+    @TaskAction
+    fun exec() {
+        val command: String = command ?: throw Exception("Please specify command")
+        command.split("\n").map { it.trim() }.filter { it != "" }.forEach { commandUnit ->
+            println("> $commandUnit")
+            val process: Process = Runtime.getRuntime().exec(arrayOf("bash", "-c", commandUnit))
+            process.inputStream.bufferedReader().use {
+                while (true) {
+                    println(it.readLine() ?: break)
+                }
+            }
+            process.errorStream.bufferedReader().use {
+                while (true) {
+                    System.err.println(it.readLine() ?: break)
+                }
+            }
+            val exitCode: Int = process.waitFor()
+            if (exitCode != 0) {
+                throw RuntimeException("""
+                    "$command" exited with non-zero exit code: $exitCode
+                """.trimIndent())
+            }
+        }
+    }
+}
+
+val assembledInstaller = file("build/installer")
+task("assemblyInstallerDir", BashExec::class) {
+    setDependsOn(listOf("jar"))
+    command = """
+        mkdir -p $assembledInstaller
+        cp distribution/installer/* $assembledInstaller
+        cp src/main/resources/icon.png $assembledInstaller
+        cp ${jar.archivePath} $assembledInstaller
+    """.trimIndent()
+}
+
+val tarFile = file("build/tar/$appName-v$appVersion.tar")
+task("tar", BashExec::class) {
+    group = "Distribution"
+    description = "Build tar archive for distribution."
+    setDependsOn(listOf("assemblyInstallerDir"))
+    val tmpDir = file("build/tmp/$appName-v$appVersion")
+    command = """
+        rm -rf $tmpDir
+            cp -r $assembledInstaller $tmpDir
+            mkdir -p ${tarFile.parent}
+            tar -cf $tarFile -C build/tmp/ ${tmpDir.name}
+        rm -rf $tmpDir
+    """.trimIndent()
+}
+
+val debFile = file("build/deb/$appName-v$appVersion.deb")
+task("deb", BashExec::class) {
+    group = "Distribution"
+    description = "Build deb archive for distribution."
+    setDependsOn(listOf("assemblyInstallerDir"))
+    val tmpDir = file("build/tmp/deb")
+    command = """
+        rm -rf $tmpDir
+            mkdir -p $tmpDir
+            $assembledInstaller/install.sh $tmpDir
+            mkdir -p $tmpDir/DEBIAN
+            cat distribution/debian/control > $tmpDir/DEBIAN/control
+            echo 'Version: $appVersion' >> $tmpDir/DEBIAN/control
+            mkdir -p ${debFile.parent}
+            dpkg-deb --build $tmpDir $debFile
+        rm -rf $tmpDir
+    """.trimIndent()
 }
