aac583f684874c3ddcd687207e20fef770a51d3c
Nikita Bobko <nikitabobko+git@gmail.com>
2020-02-03 20:38:02 +0300

    Replace `assert` with `assertTrue`

--- src/test/kotlin/ru/nikitabobko/gcalnotifier/ApplicationIdentificationTests.kt
+++ src/test/kotlin/ru/nikitabobko/gcalnotifier/ApplicationIdentificationTests.kt
@@ -4,6 +4,6 @@ import junit.framework.TestCase
 
 class ApplicationIdentificationTests : TestCase() {
   fun `test fetching APPLICATION_VERSION doesn't throw exception and version is not empty`() {
-    assert(APPLICATION_VERSION.isNotEmpty())
+    assertTrue(APPLICATION_VERSION.isNotEmpty())
   }
 }
