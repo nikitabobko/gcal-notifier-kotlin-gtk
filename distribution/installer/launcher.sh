e8ecca16d6278457e06980ef363de0447b72aabe
Nikita Bobko <nikitabobko@gmail.com>
2020-02-26 02:23:47 +0300

    Change default memory settings for Java VM
    Now we require more because user can choose how many elements
    show in popup menu and it requires more memory

--- distribution/installer/launcher.sh
+++ distribution/installer/launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 java -cp /usr/share/java/gtk.jar:/opt/gcal-notifier-kotlin-gtk/gcal-notifier-kotlin-gtk.jar \
--Xmx8m -Xms8m -Xss228k ru.nikitabobko.gcalnotifier.MainKt $@
+-Xmx10m -Xss1m ru.nikitabobko.gcalnotifier.MainKt $@
