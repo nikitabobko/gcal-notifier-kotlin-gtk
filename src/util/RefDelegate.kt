7a2b6d1fef94b58391f175509fbd4181a5ff5cd2
Nikita Bobko <git@bobko.xyz>
2022-06-15 01:37:46 +0200

    Clean-up RefDelegate implementation

--- src/util/RefDelegate.kt
+++ src/util/RefDelegate.kt
@@ -1,23 +1,14 @@
 package bobko.gcalnotifier.util
 
 import java.lang.ref.Reference
-import java.lang.ref.SoftReference
 import java.lang.ref.WeakReference
-import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-/**
- * Represents read only delegate for Java's [Reference]
- */
-interface ReadOnlyRefDelegate<T> : ReadOnlyProperty<Any?, T> {
-  val value: T
-}
-
-interface ReadWriteRefDelegate<T> : ReadOnlyRefDelegate<T>, ReadWriteProperty<Any?, T>
-
-private class SynchronizedRefDelegateUsingFun<R: Reference<T>, T>(private val refInit: (T) -> R,
-                                                                  private val init: () -> T) : ReadWriteRefDelegate<T> {
+private class RefDelegate<R: Reference<T>, T>(
+  private val refInit: (T) -> R,
+  private val init: () -> T
+) : ReadWriteProperty<Any?, T> {
   @Volatile
   private var ref: R? = null
 
@@ -26,30 +17,8 @@ private class SynchronizedRefDelegateUsingFun<R: Reference<T>, T>(private val re
     ref = refInit(value)
   }
 
-  override val value: T
-    get() = ref?.get() ?: synchronized(this) { ref?.get() ?: init().also { ref = refInit(it) } }
-
-  override fun getValue(thisRef: Any?, property: KProperty<*>): T = value
+  override fun getValue(thisRef: Any?, property: KProperty<*>): T =
+    ref?.get() ?: synchronized(this) { ref?.get() ?: init().also { ref = refInit(it) } }
 }
 
-private class SynchronizedRefDelegateUsingObj<R: Reference<T?>, T>(private val refInit: (T?) -> R,
-                                                                   obj: T) : ReadWriteRefDelegate<T?> {
-  override val value: T?
-    get() = ref.get()
-
-  @Volatile
-  private var ref: R = refInit(obj)
-    @Synchronized get() = field
-    @Synchronized private set
-
-  override fun getValue(thisRef: Any?, property: KProperty<*>): T? = value
-
-  override fun setValue(thisRef: Any?, property: KProperty<*>, value: T?) {
-    ref = refInit(value)
-  }
-}
-
-fun <T> weakRef(init: () -> T): ReadWriteRefDelegate<T> = SynchronizedRefDelegateUsingFun(::WeakReference, init)
-fun <T> softRef(init: () -> T): ReadWriteRefDelegate<T> = SynchronizedRefDelegateUsingFun(::SoftReference, init)
-fun <T> weakRefOf(obj: T): ReadWriteRefDelegate<T?> = SynchronizedRefDelegateUsingObj(::WeakReference, obj)
-fun <T> softRefOf(obj: T): ReadWriteRefDelegate<T?> = SynchronizedRefDelegateUsingObj(::SoftReference, obj)
+fun <T> weakRef(init: () -> T): ReadWriteProperty<Any?, T> = RefDelegate(::WeakReference, init)
